b935403974b641804b5327eb7c635a741b13d568
Marius Orcsik <marius@habarnam.ro>
2015-09-14 16:08:09 +0200

    Eh

--- htdocs/app_dev.php
+++ htdocs/app_dev.php
@@ -7,7 +7,6 @@ $sContent 	= '';
 date_default_timezone_set('UTC');
 
 error_reporting (E_ALL); ini_set('display_errors', 1);
-$_SERVER['REQUEST_URI'] = '/';
 
 chdir(dirname(__FILE__) . '/../');
 if ( file_exists('vendor/autoload.php') ) {
