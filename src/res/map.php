55dd263935fce0302fd0d4cdd6329296a52aa35e
Marius Orcsik <marius@habarnam.ro>
2016-01-06 14:57:48 +0100

    Added year only url mappings
    Explicitely marking favicon.ico as 404

--- src/res/map.php
+++ src/res/map.php
@@ -1,12 +1,14 @@
 <?php
 /** @var $this \vsc\application\sitemaps\SiteMapA */
+use vsc\application\processors\NotFoundProcessor;
+
 $this->getCurrentModuleMap()->map('\.ics$', \tlcal\application\controllers\ICalController::class);
 $this->getCurrentModuleMap()->map('\.txt$', \tlcal\application\controllers\PlainTextController::class);
 
 $html = $this->getCurrentModuleMap()->map('.*', \vsc\application\controllers\Html5Controller::class);
 $html->setMainTemplatePath('templates');
 $html->setMainTemplate('main.tpl.php');
-
+$this->map('.*favicon.ico', NotFoundProcessor::class);
 // http://calendar/starcraft/2015/november/13/
 $this->map('(\w+)/(\d{4})/(\w{3,})/(\d{1,2})/?', \tlcal\application\processors\Calendar::class);
 // http://calendar/starcraft/2015/11/13/
@@ -16,6 +18,9 @@ $this->map('(\w+)/(\d{4})/(\d{1,2})/(\d{1,2})/?', \tlcal\application\processors\
 $this->map('(\w+)/(\d{4})/(\w{3,})/?', \tlcal\application\processors\Calendar::class);
 // http://calendar/starcraft/2015/11/
 $this->map('(\w+)/(\d{4})/(\d{1,2})/?', \tlcal\application\processors\Calendar::class);
+
+// http://calendar/starcraft/2015/
+$this->map('(\w+)/(\d{4})/?', \tlcal\application\processors\Calendar::class);
 
 $this->map('(\w+)/?', \tlcal\application\processors\Calendar::class);
 $this->map('.*/?', \tlcal\application\processors\Calendar::class);
