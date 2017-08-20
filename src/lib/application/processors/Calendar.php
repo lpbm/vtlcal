de51172e57407d6f3df1f80fefe257c3efdb86af
Marius Orcsik <marius@habarnam.ro>
2017-08-20 17:12:45 +0200

    Improve(?) the calendar type from uri method

--- src/lib/application/processors/Calendar.php
+++ src/lib/application/processors/Calendar.php
@@ -27,80 +27,90 @@ class Calendar extends ProcessorA
         $var = strtolower($var);
         $calendars = [];
 
-        if ($var == 'dota') {
-            $calendars[] = 'dot';
-        }
-        if ($var == 'cs'  || $var == 'cs:go' || $var == 'csgo' || $var == 'counter' || $var == 'counterstrike') {
-            $calendars[] = 'csg';
-        }
-        if ($var == 'league' || $var == 'lol') {
-            $calendars[] = 'lol';
-        }
-        if ($var == 'hearthstone') {
-            $calendars[] = 'hrt';
-        }
-        if ($var == 'starcraft' || $var == 'sc2') {
-            $calendars[] = 'sc2';
-        }
-        if ($var == 'broodwar' || $var == 'bw') {
-            $calendars[] = 'brw';
-        }
-        if ($var == 'smash') {
-            $calendars[] = 'sms';
-        }
-        if ($var == "pfw") {
-            $calendars[] = 'pfw';
-        }
-        if ($var == "qlv") {
-            $calendars[] = 'qlv';
-        }
-        if ($var == "qiv") {
-            $calendars[] = 'qiv';
-        }
-        if ($var == "qiii") {
-            $calendars[] = 'qiii';
-        }
-        if ($var == "qii") {
-            $calendars[] = 'qii';
-        }
-        if ($var == "qw") {
-            $calendars[] = 'qw';
-        }
-        if ($var == "dbt") {
-            $calendars[] = 'dbt';
-        }
-        if ($var == "doom") {
-            $calendars[] = 'doom';
-        }
-        if ($var == "rfl") {
-            $calendars[] = 'rfl';
-        }
-        if ($var == "ovw") {
-            $calendars[] = 'ovw';
-        }
-        if ($var == "gg") {
-            $calendars[] = 'gg';
-        }
-        if ($var == "ut") {
-            $calendars[] = 'ut';
-        }
-        if ($var == "wsw") {
-            $calendars[] = 'wsw';
-        }
-        if ($var == "dbmb") {
-            $calendars[] = 'dbmb';
-        }
-        if ($var == "xnt") {
-            $calendars[] = 'xnt';
-        }
-        if ($var == "qch") {
-            $calendars[] = 'qch';
-        }
-        if ($var == "cpma") {
-            $calendars[] = 'cpma';
+        if (LiquidAssets::validType($var)) {
+            $calendars[] = $var;
+        } else {
+            if ($var == 'dota') {
+                $calendars[] = LiquidAssets::DOT_ID;
+            }
+            if ($var == 'cs' || $var == 'cs:go' || $var == 'csgo' || $var == 'counter' || $var == 'counterstrike') {
+                $calendars[] = LiquidAssets::CSG_ID;
+            }
+            if ($var == 'league') {
+                $calendars[] = LiquidAssets::LOL_ID;
+            }
+            if ($var == 'hearthstone') {
+                $calendars[] = LiquidAssets::HRT_ID;
+            }
+            if ($var == 'starcraft') {
+                $calendars[] = LiquidAssets::SC2_ID;
+            }
+            if ($var == 'broodwar' || $var == 'bw') {
+                $calendars[] = LiquidAssets::BRW_ID;
+            }
+            if ($var == 'smash') {
+                $calendars[] = LiquidAssets::SMA_ID;
+            }
+            if ($var == "quake:live") {
+                $calendars[] = LiquidAssets::QLV_ID;
+            }
+            if ($var == "quake:iv") {
+                $calendars[] = LiquidAssets::QIV_ID;
+            }
+            if ($var == "quake:iii") {
+                $calendars[] = LiquidAssets::QIII_ID;
+            }
+            if ($var == "quake:ii") {
+                $calendars[] = LiquidAssets::QII_ID;
+            }
+            if ($var == "quake:world") {
+                $calendars[] = LiquidAssets::QW_ID;
+            }
+            if ($var == "diabotical") {
+                $calendars[] = LiquidAssets::DBT_ID;
+            }
+            if ($var == "overwatch") {
+                $calendars[] = LiquidAssets::OVW_ID;
+            }
+            if ($var == "unreal") {
+                $calendars[] = LiquidAssets::UT_ID;
+            }
+            if ($var == "warsow") {
+                $calendars[] = LiquidAssets::WSW_ID;
+            }
+            if ($var == "xonotic") {
+                $calendars[] = LiquidAssets::XNT_ID;
+            }
+            if ($var == "quake:champions") {
+                $calendars[] = LiquidAssets::QCH_ID;
+            }
+            if ($var == "tl") {
+                $calendars[] = LiquidAssets::SC2_ID;
+                $calendars[] = LiquidAssets::BRW_ID;
+                $calendars[] = LiquidAssets::CSG_ID;
+                $calendars[] = LiquidAssets::SMA_ID;
+            }
+            if ($var == "pfw") {
+                $calendars[] = LiquidAssets::QLV_ID;
+                $calendars[] = LiquidAssets::QIV_ID;
+                $calendars[] = LiquidAssets::QIII_ID;
+                $calendars[] = LiquidAssets::QII_ID;
+                $calendars[] = LiquidAssets::QW_ID;
+                $calendars[] = LiquidAssets::DBT_ID;
+                $calendars[] = LiquidAssets::DOOM_ID;
+                $calendars[] = LiquidAssets::RFL_ID;
+                $calendars[] = LiquidAssets::OVW_ID;
+                $calendars[] = LiquidAssets::GG_ID;
+                $calendars[] = LiquidAssets::UT_ID;
+                $calendars[] = LiquidAssets::WSW_ID;
+                $calendars[] = LiquidAssets::DBMB_ID;
+                $calendars[] = LiquidAssets::XNT_ID;
+                $calendars[] = LiquidAssets::QCH_ID;
+                $calendars[] = LiquidAssets::CPMA_ID;
+            }
         }
         if (count($calendars) == 0) {
-            $calendars[] = 'all';
+            $calendars[] = LiquidAssets::ALL_ID;
         }
         return $calendars;
     }
@@ -112,6 +122,7 @@ class Calendar extends ProcessorA
         $year = $this->getVar('year');
         $month = $this->getVar('month');
         $day = $this->getVar('day');
+        $interval = 'P1Y';
 
         if (is_null($day)) {
             $interval = 'P1M';
